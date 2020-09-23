d11eb9b53d486563ab963cecf9c53bdf22f14e10
Nolan Picini <nolanpic@gmail.com>
2020-09-23 15:54:43 -0700

    add routes/pages for matched, unmatched

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import clients from "./data/clients";
 import experts from "./data/experts";
+import { NavLink, Switch, Route } from "react-router-dom";
 import ClientContext from "./contexts/ClientContext";
 import ExpertContext from "./contexts/ExpertContext";
 import MatchContext from "./contexts/MatchContext";
+import Unmatched from "./pages/Unmatched";
+import Matched from "./pages/Matched";
 
 function App() {
   // set up state to pass to providers
@@ -21,20 +24,18 @@ function App() {
       <ClientContext.Provider value={[unmatchedClients, setUnmatchedClients]}>
         <MatchContext.Provider value={[matched, setMatched]}>
           <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </header>
+            <NavLink to="/">Unmatched</NavLink>
+            <NavLink to="matched">Matched</NavLink>
+            <div className="container">
+              <Switch>
+                <Route path="/matched">
+                  <Matched />
+                </Route>
+                <Route path="/">
+                  <Unmatched />
+                </Route>
+              </Switch>
+            </div>
           </div>
         </MatchContext.Provider>
       </ClientContext.Provider>
