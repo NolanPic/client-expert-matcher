3de0ad45b3abbc2a8dbb031e5184b073047433c9
Nolan Picini <nolanpic@gmail.com>
2020-09-23 16:50:31 -0700

    add IDs to clients + experts

--- src/data/clients.js
+++ src/data/clients.js
@@ -1,61 +1,76 @@
 export default [
   {
+    id: 1,
     firstName: "Zackary",
     lastName: "Leon",
   },
   {
+    id: 2,
     firstName: "Preston",
     lastName: "Peterson",
   },
   {
+    id: 3,
     firstName: "Casey",
     lastName: "Hunter",
   },
   {
+    id: 4,
     firstName: "Monica",
     lastName: "Adkins",
   },
   {
+    id: 5,
     firstName: "Simon",
     lastName: "Wong",
   },
   {
+    id: 6,
     firstName: "Eric",
     lastName: "Acosta",
   },
   {
+    id: 7,
     firstName: "Olivia",
     lastName: "Delgado",
   },
   {
+    id: 8,
     firstName: "Katie",
     lastName: "Valdez",
   },
   {
+    id: 9,
     firstName: "Hannah",
     lastName: "Molina",
   },
   {
+    id: 10,
     firstName: "Tristan",
     lastName: "Marsh",
   },
   {
+    id: 11,
     firstName: "Kate",
     lastName: "Lawson",
   },
   {
+    id: 12,
     firstName: "Andres",
     lastName: "Lee",
   },
   {
+    id: 13,
     firstName: "Cameron",
     lastName: "Johnson",
   },
   {
+    id: 14,
     firstName: "Esther",
     lastName: "Rhodes",
   },
   {
+    id: 15,
     firstName: "Clayton",
     lastName: "Sherman",
   },
--- src/data/experts.js
+++ src/data/experts.js
@@ -1,21 +1,26 @@
 export default [
   {
+    id: 1,
     firstName: "Tyson",
     lastName: "Powers",
   },
   {
+    id: 2,
     firstName: "Tyrone",
     lastName: "Strickland",
   },
   {
+    id: 3,
     firstName: "Billy",
     lastName: "Blair",
   },
   {
+    id: 4,
     firstName: "Joey",
     lastName: "Kelley",
   },
   {
+    id: 5,
     firstName: "Nicolas",
     lastName: "Harrington",
   },
