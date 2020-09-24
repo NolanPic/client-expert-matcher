96b58c573c8728788b0a6661527a0f3334995ac8
Nolan Picini <nolanpic@gmail.com>
2020-09-23 17:39:23 -0700

    rough in Matched page

--- src/pages/Matched.js
+++ src/pages/Matched.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import MatchContext from "../contexts/MatchContext";
 
 const Matched = () => {
-  return <div>Matched</div>;
+  const [matches] = useContext(MatchContext);
+
+  return matches.length ? (
+    <ul className="match-list">
+      {matches.map((match) => (
+        <li key={match.client.id}>
+          {match.client.firstName + " " + match.client.lastName} is matched with{" "}
+          {match.expert.firstName + " " + match.expert.lastName}
+        </li>
+      ))}
+    </ul>
+  ) : (
+    <p>No matches have been made</p>
+  );
 };
 
 export default Matched;
